2226c00d6213b9873f751b272de6791ea34d178a
Hanna Angeria <haan2402@student.miun.se>
2025-05-06 15:30:53 +0200

    lagt till filtrering av det som skrivs in i input fält, använt ngmodule

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,16 +1,20 @@
 import { Component, inject, signal } from '@angular/core';
 import { Course } from '../models/course';
 import { CourseService } from '../services/course.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-courses',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.css'
 })
 export class CoursesComponent {
-  courses = signal<Course[]>([]);
-  error = signal<string | null>(null);
+  courses = signal<Course[]>([]); //lista från början från filen med alla kurser
+  error = signal<string | null>(null); //felmeddelande om kurser inte går att hämta
+  filteredCourses = signal<Course[]>([]); //till listan med filtrering av kurser 
+  filterValue: string = ''; //string för att ha en textsträng från input fältet
 
   courseService = inject(CourseService);
 
@@ -18,14 +22,29 @@ export class CoursesComponent {
     this.loadCourses();
   }
 
+  //hämtar in kurserna och filtrerade kurser
   async loadCourses() {
     try {
       const response = await this.courseService.loadCourses();
       this.courses.set(response);
+      this.filteredCourses.set(response);
       console.table(this.courses());
     } catch(error) {  
       console.error(error);
       this.error.set("Problem med att ladda in data - testa igen om en stund!");
     }
   }
+
+  //filtrerar de olika kurserna beroende på vad man skriver in i input-fältet, sedan uppdaterar listan 
+  applyFilter(): void {
+    const filterCourse = this.courses().filter(course =>
+      course.code.toLowerCase().includes(this.filterValue.toLocaleLowerCase()) ||
+      course.coursename.toLocaleLowerCase().includes(this.filterValue.toLocaleLowerCase())
+    );
+    this.filteredCourses.set(filterCourse);
+  }
+  
+
+
+  
 }
